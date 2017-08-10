85ee6813bd4a07bdd37387b9d425aac1dba51376
Toshiaki Ara (Univ) <ara_t@po.mdu.jp>
2017-08-10 09:13:49 +0900

    delete duplicated function

--- CIplot.R
+++ CIplot.R
@@ -1,5 +1,4 @@
-CIplot <-
-    function(x, ...) UseMethod("CIplot")
+CIplot <- function(x, ...) UseMethod("CIplot")
 
 CIplot.htest <-
     function(x,
@@ -140,24 +139,6 @@ CIplot.OddsRatio <-
            col = pcol, bg = pcolbg)
 
     box()
-}
-
-
-CI.posthocTGH <- function(x)
-{
-  if (x$input$method == 'tukey') {
-      tmp <- x$output$tukey;
-  }
-  else if (x$input$method == 'games-howell') {
-      tmp <- x$output$games.howell;
-  }
-
-  res <- as.matrix(tmp[, 1:6])
-  colnames(res)[2:3] <- c("lwr", "upr")
-
-  attr(res, "conf.level") <- x$input$conf.level
-  attr(res, "class") <- "Tukey"
-  return(res)
 }
 
 
