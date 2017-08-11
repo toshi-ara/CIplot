438b65ed8ebf2942705b58b131a25553c2406f02
Toshiaki Ara <ara_t@384.jp>
2017-08-11 13:22:15 +0900

    add CIplot.common / rewrite using CIplot.common

--- R/CIplot.R
+++ R/CIplot.R
@@ -1,63 +1,48 @@
 CIplot <-
     function(x, ...) UseMethod("CIplot")
 
-CIplot.htest <-
+## x: data.frame(estimation, lwr, upr)
+##    atr(x, "conf.level")
+## requires: v
+CIplot.common <-
     function(x,
-             log = FALSE,
-             xlim = NULL, xlab = NULL, main = NULL,
-             pch = 21, pcol = "black", pcolbg = "white", pcex = 1,
-             cilty = 1, cilwd = 1, cicol = "black",
-             v = NULL, vlty = 2, vlwd = 1,  vcol = "black",
+             xlog = FALSE, xlim = NULL, xlab = NULL,             ## x-axis
+             yname = TRUE, las = 0,                              ## y-axis
+             pch = 21, pcol = 1, pcolbg = "white", pcex = 1,     ## points
+             conf.level = 0.95, cilty = 1, cilwd = 1, cicol = 1, ## conf.int
+             v, vlty = 2, vlwd = 1,  vcol = 1,                   ## vertical
+             main = NULL,                                        ## Title
              ...)
 {
-    est <- x$estimate
-    ci <- x$conf.int
-    if (is.null(ci)) {
-        warning("No information about confidential interval\n")
-        return(invisible(NULL))
-    }
-
-    if (length(est) == 2L) est <- est[1] - est[2]
-
-    if (is.null(v)) {
-        if (is.null(x$null.value)) {
-            v <- ifelse(log, 1, 0)
-        } else {
-            v <- x$null.value
-        }
-    }
-
-    if (is.null(xlim)) {
-        xlim <- c(min(v, ci[1]), max(v, ci[2]))
-    }
-
     if (is.null(main)) {
-        main <- paste0(format(100 * attr(ci, "conf.level"), digits = 2L),
+        main <- paste0(format(100 * conf.level, digits = 2L),
                        "% confidence interval")
     }
 
-    if (is.null(xlab)) {
-        xlab <- attr(x$null.value, "names")
-        if (is.null(xlab)) {
-            xlab <- ifelse(log, "ratio", "difference")
-        }
+    xi <- x[,1:3]         ## estimation, lwr, upr
+    yvals <- nrow(xi):1L
+
+    if (is.null(xlim)) xlim <- c(min(xi), max(xi))
+
+    plot(c(xi[,"lwr"], xi[,"upr"]), rep.int(yvals, 2L),
+         type = "n", axes = FALSE,
+         xlab = xlab, ylab = "",
+         xlim = xlim, log = ifelse(xlog, "x", ""),
+         ylim = c(0.5, nrow(xi) + 0.5),
+         main = main)
+
+    axis(1, ...)
+    if (yname) {
+        axis(2, at = nrow(xi):1,
+             labels = dimnames(xi)[[1L]], las = las)
     }
 
-    plot(ci, rep(1, 2), type = "n", axes = FALSE,
-         xlim = xlim,
-         xlab = xlab, ylab = "", yaxt = "n",
-         main = main,
-         log = ifelse(log, "x", ""),
-         ...)
-    axis(1, ...)
-
-    arrows(ci[1], 1, ci[2], 1, angle = 90, code = 3,
+    arrows(xi[, "lwr"], yvals, xi[, "upr"], yvals,
+           code = 3, angle = 90, length = 0.15,
            lty = cilty, lwd = cilwd, col = cicol)
-
-    points(est, 1, pch = pch, cex = pcex,
+    points(xi[, 1], yvals, pch = pch, cex = pcex,
            col = pcol, bg = pcolbg)
     abline(v = v, lty = vlty, lwd = vlwd, col = vcol)
-
     box()
 }
 
